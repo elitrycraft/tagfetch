4afedcf53507fc6c1f251d8afc1a6f7182a3b0ba
Deniz1111212 <elitrycraft@gmail.com>
2026-07-21 23:07:56 +0300

    Implement TagFetch installation in get.sh
    Add installation script for TagFetch on Linux

--- get.sh
+++ get.sh
@@ -1,1 +1,8 @@
+echo "Installing TagFetch for Linux"
+sudo wget https://github.com/elitrycraft/tagfetch/releases/download/1.0.3R/TagFetch_linux_x86_64 -O "/usr/bin/tagfetch"
+sudo chmod +x /usr/bin/tagfetch
 
+if [ -f /usr/bin/tagfetch ]; then
+  echo "TagFetch has been successfully installed."
+else
+  echo "TagFetch not installed"
