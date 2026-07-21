c53490ec4949369afb526467a9c10a31cd4bbc0f
Deniz1111212 <elitrycraft@gmail.com>
2026-07-21 23:12:13 +0300

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,5 +1,5 @@
 echo "Installing TagFetch for Linux..."
-sudo wget https://github.com/elitrycraft/tagfetch/releases/download/1.0.3R/TagFetch_linux_x86_64 -O "/usr/bin/tagfetch"
+sudo wget --show-progress https://github.com/elitrycraft/tagfetch/releases/download/1.0.3R/TagFetch_linux_x86_64 -O "/usr/bin/tagfetch"
 sudo chmod +x /usr/bin/tagfetch
 
 echo "TagFetch has been successfully installed."
