971baf091bc34ac41531a93f9b43e2016ebbbd63
Deniz1111212 <elitrycraft@gmail.com>
2026-07-22 21:53:36 +0300

    Update get.bat

--- get.bat
+++ get.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Requesting administrator privileges...
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    powershell -Command "Start-Process '%~f0' -Verb RunAs -WindowStyle Normal"
     exit /b
 )
 
