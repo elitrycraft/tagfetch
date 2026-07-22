cc580108e974be4aa3354abd3516d4f2e9a19096
Deniz1111212 <elitrycraft@gmail.com>
2026-07-22 21:44:08 +0300

    Update get.bat

--- get.bat
+++ get.bat
@@ -6,7 +6,8 @@ net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Requesting administrator privileges...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
+    set "scope=Machine"
+    set "dest=%ProgramFiles%\TagFetch"
 )
 
 :: detect arch
